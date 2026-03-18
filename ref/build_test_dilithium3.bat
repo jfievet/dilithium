4fcd2a5590f59e82b5a80ae596fe7801c1279945
jfievet <jfievet@protonmail.com>
2026-03-18 14:44:11 +0100

    Add Windows batch files for compiling test_dilithium

---
+++ ref/build_test_dilithium3.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM ---- Compiler ----
+set CC=gcc
+
+REM ---- Flags ----
+set CFLAGS=-Wall -Wextra -Wpedantic -Wmissing-prototypes -Wredundant-decls -Wshadow -Wvla -Wpointer-arith -O3 -fomit-frame-pointer
+
+REM ---- Sources ----
+set SOURCES=sign.c packing.c polyvec.c poly.c ntt.c reduce.c rounding.c
+set KECCAK_SOURCES=%SOURCES% fips202.c symmetric-shake.c
+
+%CC% %CFLAGS% -DDILITHIUM_MODE=3 ^
+ -o test\test_dilithium3.exe ^
+ test\test_dilithium.c randombytes.c %KECCAK_SOURCES%
+
+echo Done.
